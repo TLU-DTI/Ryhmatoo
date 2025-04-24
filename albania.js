a8636e10e5dc835de1dd4b35e2e66f19bde723d9
Eliana <146323849+leonova19@users.noreply.github.com>
2025-04-24 14:21:47 +0300

    albania.change

---
+++ albania.js
@@ -0,0 +1,28 @@
+
+  const track = document.querySelector('.carousel-track');
+  const images = document.querySelectorAll('.carousel img');
+  const prevBtn = document.querySelector('.carousel-button.prev');
+  const nextBtn = document.querySelector('.carousel-button.next');
+
+  let currentIndex = 0;
+
+  function updateCarousel() {
+    const imageWidth = images[0].clientWidth;
+    track.style.transform = `translateX(-${currentIndex * imageWidth}px)`;
+  }
+
+  nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateCarousel();
+  });
+
+  prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateCarousel();
+  });
+
+  // обновление при загрузке, на всякий случай
+  window.addEventListener('resize', updateCarousel);
+
+
+
